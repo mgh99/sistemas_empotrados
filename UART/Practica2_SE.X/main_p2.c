6757f90ab802cdb2a52fe0c1c7e8f0ecd99fc8db
María González Herrero <mgonzalezh3@alumnos.nebrija.es>
2021-10-29 02:27:45 +0200

    upload proyecto

--- UART/Practica2_SE.X/main_p2.c
+++ UART/Practica2_SE.X/main_p2.c
@@ -1,6 +1,7 @@
 /*
- * File:   main_principal.c
+ * File:   main_p2.c
  * Author: María González
+ *         Santiago Molpeceres
  *
  * Created on 24 de octubre de 2021, 17:42
  */
@@ -49,142 +50,148 @@
 
 #include <xc.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
 
 #define baud_9600 1041 // Es este valor porque sale de la fórmula de UxBRG 40 * 106 / 4 * 1960
-#define LED_RED  LATBbits.LATB7 //D1
-#define LED_GREEN LATAbits.LATA7 //D2
+#define LED_RED  LATBbits.LATB3 //D1  RB3
+#define LED_GREEN LATAbits.LATA0 //D2 RA0
 
-void delay_ms(unsigned long time_ms) 
-{
+void delay_ms(unsigned long time_ms) {
     unsigned long u;
-    for(u = 0; u < time_ms * 900; u++) // Calculo aproximado para una CPU a 4MHz
+    for (u = 0; u < time_ms * 900; u++) // Calculo aproximado para una CPU a 4MHz
     {
         asm("NOP");
     }
 }
 
-void uart_config(unsigned int baud) 
-{
+void uart_config(unsigned int baud) {
     //Asignar módulo UART a un puerto/pines de comunicacion
-    TRISCbits.TRISC0 =   1; // Pin RCO como puerto de entrada (digital)
-    //RPINR18bits.U1RXR = 16; // pin RC0 al puerto de recepcion uart 1
-    RPINR18bits.U1RXR = 23; // pin RC0 al puerto de recepcion uart 1
-    RPOR8bits.RP17R =    3; // Pin RC1 conectado al pin de transmision de la uart1
     
+    TRISCbits.TRISC0 = 1;                // Pin RCO como puerto de entrada (digital)
+    RPINR18bits.U1RXR = 16;               // pin RC0 al puerto de recepcion uart 1   RP16
+    RPOR8bits.RP17R = 3;                 // Pin RC1 conectado al pin de transmision de la uart1
+
     //Configuración registro U1MODE
     U1MODEbits.UARTEN = 0; // Al inicio está deshabilitada la UART
-    U1MODEbits.USIDL =  0; // Continua la operación aunque se encuentre en el modo IDLE
-    U1MODEbits.IREN =   0; // IR no se usa
-    U1MODEbits.RTSMD =  1; // Control de flujo descativado
-    U1MODEbits.UEN =    0; // Solo usamo pin TX (transmisión) y Rx (recepción) para nuestro proyecto
-    U1MODEbits.WAKE =   0; // Para que la UART no se "despierte" del modo sleep
+    U1MODEbits.USIDL = 0; // Continua la operación aunque se encuentre en el modo IDLE
+    U1MODEbits.IREN = 0; // IR no se usa
+    U1MODEbits.RTSMD = 1; // Control de flujo descativado
+    U1MODEbits.UEN = 0; // Solo usamo pin TX (transmisión) y Rx (recepción) para nuestro proyecto
+    U1MODEbits.WAKE = 0; // Para que la UART no se "despierte" del modo sleep
     U1MODEbits.LPBACK = 0; // Loopback deshabilitado
-    U1MODEbits.ABAUD =  0; // Auto baud rate deshabilitado
+    U1MODEbits.ABAUD = 0; // Auto baud rate deshabilitado
     U1MODEbits.URXINV = 0; // El estado en reposo (IDLE) es un 1(alto)
-    U1MODEbits.BRGH =   1; // Modo high-speed
-    U1MODEbits.PDSEL =  0; // 8 bits de datos y paridad nula (8N)
-    U1MODEbits.STSEL =  0; // 1-bit de stop al final de la trama de datos(8N1)
-    
+    U1MODEbits.BRGH = 1; // Modo high-speed
+    U1MODEbits.PDSEL = 0; // 8 bits de datos y paridad nula (8N)
+    U1MODEbits.STSEL = 0; // 1-bit de stop al final de la trama de datos(8N1)
+
     //Configuración registro U1STA
     U1STAbits.URXISEL0 = 0;
-    U1STAbits.URXISEL1 = 0; 
-    U1STAbits.URXISEL =  0; // Tema interrumpciones. No mirar aun
-    U1STAbits.ADDEN =    0; // No usamos direccionamiento
-    U1STAbits.UTXBRK =   0; // No usar trama de sincronizacion
-    U1STAbits.UTXINV =   0; // Estado de reposo del pin de transmisión es por defecto 1 _> High
-    U1STAbits.OERR =     0; // Buffer de recepcion esta vacio y no hay problema de overflow
-    U1STAbits.PERR =     0;
-    U1STAbits.FERR =     0;
-    U1STAbits.UTXEN =    1; //El transmisor está en pleno funcionamiento
-    
+    U1STAbits.URXISEL1 = 0;
+    U1STAbits.URXISEL = 0; // Tema interrumpciones. No mirar aun
+    U1STAbits.ADDEN = 0; // No usamos direccionamiento
+    U1STAbits.UTXBRK = 0; // No usar trama de sincronizacion
+    U1STAbits.UTXINV = 0; // Estado de reposo del pin de transmisión es por defecto 1 _> High
+    U1STAbits.OERR = 0; // Buffer de recepcion esta vacio y no hay problema de overflow
+    U1STAbits.PERR = 0;
+    U1STAbits.FERR = 0;
+    U1STAbits.UTXEN = 1; //El transmisor está en pleno funcionamiento
+
     //Configuramos la velocidad de transmisión/recepción de los datos
     U1BRG = baud;
-    
+
     U1MODEbits.UARTEN = 1; // UART habilitada
 }
 
-int main(void) {
-    
-    //Fosc = Fon * (M1/(N1*N2))
-    //fOSC = 8Mhz * (40/(2*2))
-    
-    PLLFBD = 38;                       // M = 40
-    CLKDIVbits.PLLPOST = 0;           // N1 = 2
-    CLKDIVbits.PLLPRE = 0;            // N2 = 2;
-    while(OSCCONbits.LOCK != 1);      // Wait for PLL to LOCK
-    
-    AD1PCFGL = 0XFFFF; // Todos los pines configurados en digital
-    
-    TRISBbits.TRISB7 = 0; // Configuramos el pin RB3 del puerto B como salida (D1)
-    TRISAbits.TRISA7 = 0; // Configuramos el pin RA0 del puerto A como salida (D2)
-    
-    delay_ms(10); // Hacemos una pequeña espera para asegurar que se ha hecho la configuración antes de continuar
-    
-    LATBbits.LATB7 = 0; // Estado 'low', por defecto, en el pin RB3 (D1)
-    LATAbits.LATA0 = 0; // Estado 'low', por defecto, en el pin RA0 (D2)
-    
-    delay_ms(10); // Hacemos una pequeña espera para asegurar que se ha hecho la configuración antes de continuar
-    uart_config(baud_9600);
-    
-    // Variables necesarias
-    int contador = '0';
-    char nombre[] = "Maria";
-    int j = 0;
-    bool pulsado = false;  
-    bool ms500 = true; 
-    
-    while(j < sizeof(nombre)){
-        U1TXREG = nombre[j];
+void imprime_cadena(char cadena[]) {
+    int i = 0;
+    char* j = &cadena[0];
+    while (U1STAbits.UTXBF == 0) {
+        U1TXREG = (char) (*j);
+        i++;
         j++;
     }
-    
-    while(1) 
-    {
-        // Ejercicio 1
-        if(ms500 == true)
-        {
-            U1TXREG = contador;
-            (int)contador++;
-        }
-        ms500 = !ms500;
+}
+
+void parpadeo() {
+    LED_GREEN = !PORTAbits.RA0;
+}
+
+int main(void) {
+
+    //Fosc = Fon * (M1/(N1*N2))
+    //fOSC = 8Mhz * (40/(2*2))
+
+    PLLFBD = 38; // M = 40
+    CLKDIVbits.PLLPOST = 0; // N1 = 2
+    CLKDIVbits.PLLPRE = 0; // N2 = 2;
+    while (OSCCONbits.LOCK != 1); // Wait for PLL to LOCK
+
+    AD1PCFGL = 0XFFFF; // Todos los pines configurados en digital
+
+    TRISBbits.TRISB3 = 0;               // Configuramos el pin RB3 del puerto B como salida (D1)
+    TRISAbits.TRISA0 = 0;               // Configuramos el pin RA0 del puerto A como salida (D2) 
+    delay_ms(10);                       // Hacemos una pequeña espera para asegurar que se ha hecho la configuración antes de continuar
+
+    LATBbits.LATB3 = 0;                 // Estado 'low', por defecto, en el pin RB3 (D1)
+    LATAbits.LATA0 = 0;                  // Estado 'low', por defecto, en el pin RA0 (D2) 
+    delay_ms(10);                       // Hacemos una pequeña espera para asegurar que se ha hecho la configuración antes de continuar
+    uart_config(baud_9600);
+
+    // Variables necesarias
+    bool h_pulsada = 0;
+    char ReceivedChar;
+    int contador = 0;
+    char nombre[] = "Maria";
+
+    imprime_cadena(nombre);
+
+    while (1) {
         
-        // Ejercicio 2
-        if(U1STAbits.URXDA == 1) 
-        {
-            if(U1RXREG == 69 || U1RXREG == 101) //Tecla E o e
-            {
-                LED_RED = 1; // Encender LED D2
-            } else if(U1RXREG == 65 || U1RXREG == 97) 
-            {
-                pulsado = !pulsado;
-            }
-            
-            // Ejercicio 3
-            if(U1RXREG == 72 || U1RXREG == 97) //Tecla H o h 
-            {
-                pulsado = !pulsado;
-            }
-            
-            //Ejercicio 4
-            if(U1RXREG == 32) //Tecla del espacio
-            {
-                contador = '0';
-            }
+        //EJERCICIO 1
+        char str[100];                          // no es muy optimo, llegará un punto que deje de contar por que no entra el numero
+        sprintf(str, "%d", contador);
+        imprime_cadena(str);
+        contador++;
+        delay_ms(500);                            // genera aproximadamente un delay de 0.5 sengundos 
+
+        /* mira si ha habido un error*/
+        if (U1STAbits.FERR == 1) {
+            continue;
         }
         
-        //Continuación del ejercicio 3
-        if(pulsado == true) 
-        {
-            LED_GREEN = !PORTAbits.RA7;
-        }else if(pulsado == false) 
-        {
-            LED_GREEN = 0;
+        /* soluciona el overrrun */
+        if (U1STAbits.OERR == 1) {
+            U1STAbits.OERR = 0;
+            continue;
         }
-        delay_ms(250);   
+                                                                                                 /* Get the data */
+        
+        if (U1STAbits.URXDA) {
+            ReceivedChar = (char) U1RXREG;
+            //EJERCICIO 2
+            if (ReceivedChar == 'e' || ReceivedChar == 'E') {
+                LATBbits.LATB3 = 1;
+            } else if (ReceivedChar == 'a' || ReceivedChar == 'A') {
+                LATBbits.LATB3 = 0;
+            //EJERCICIO 4
+            } else if (ReceivedChar == ' ') {
+                contador = 0;
+            //EJERCICIO 3
+            } else if (ReceivedChar == 'h' || ReceivedChar == 'H') {
+                h_pulsada = !h_pulsada;
+                if (h_pulsada == 0) {
+                    LED_GREEN = 0;
+                }
+            }
+        }
+        if (h_pulsada) {
+            parpadeo();
+        }
+        delay_ms(250);
     }
-     
     return 0;
 }
